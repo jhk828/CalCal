6534d80d22bf2368b6db4acd89bb16fbbf9324ab
Gwang-chae <kcseo25@naver.com>
2020-09-02 15:18:42 +0900

    Paging CSS

--- FoodInfo/views.py
+++ FoodInfo/views.py
@@ -186,7 +186,7 @@ def searchByDate(request):
 
     if q:
         tables = tables.filter(name__icontains=q)
-        paginator = Paginator(tables, 10)
+        paginator = Paginator(tables, 20)
         page = request.GET.get('page')
         table_pages = paginator.get_page(page)
         return render(request, 'searchByDate.html', {'tables': tables, 'q': q, 'table_pages': table_pages, 'getDate': getDate})
